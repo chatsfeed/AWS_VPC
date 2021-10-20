681a3dbbde0d70a272113cf0a2927a7fd25a46e9
chatsfeed <83351200+chatsfeed@users.noreply.github.com>
2021-10-19 21:48:54 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -593,7 +593,6 @@ resource "aws_alb_listener" "listener_load_balancer" {
 resource "aws_lb_listener_certificate" "ourservice_ssl_cert" {
   listener_arn    = aws_alb_listener.listener_load_balancer.arn
   certificate_arn = aws_acm_certificate.wildcard_website.arn
-  depends_on = [aws_alb_listener.listener_load_balancer.arn, aws_acm_certificate.wildcard_website.arn]
 }
 
 # We create a security group for our wordpress instance
