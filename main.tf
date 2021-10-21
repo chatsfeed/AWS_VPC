5ebab6486a2b3b5a53e3e4df321638b4e545ef29
chatsfeed <83351200+chatsfeed@users.noreply.github.com>
2021-10-20 22:57:53 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -122,19 +122,6 @@ resource "aws_route53_record" "website_alb_root_record" {
 }
 
 
-#resource "aws_route53_record" "cert_validation" {
-  #name = aws_acm_certificate.wildcard_website.domain_validation_options.0.resource_record_name
-  #type = aws_acm_certificate.wildcard_website.domain_validation_options.0.resource_record_type
-  #zone_id = data.aws_route53_zone.main.id
-  #records = [aws_acm_certificate.wildcard_website.domain_validation_options.0.resource_record_value]
-  #ttl = 60
-#}
-
-
-
-
-
-
 
 # We create a new VPC
 resource "aws_vpc" "vpc" {
