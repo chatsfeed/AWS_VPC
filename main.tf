680ce89d4a65261ae3ffe86c6c7fd0d37a0c9f1c
chatsfeed <83351200+chatsfeed@users.noreply.github.com>
2021-10-20 22:02:43 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -604,9 +604,8 @@ resource "aws_alb_listener" "listener_load_balancer" {
   port              = "443"
   protocol          = "HTTPS"
   
-  #ssl_policy = "ELBSecurityPolicy-2016–08"
-  #certificate_arn = data.aws_acm_certificate.wildcard_website.arn
-  certificate_arn = aws_acm_certificate_validation.wildcard_cert.certificate_arn
+  ssl_policy = "ELBSecurityPolicy-2016-08"
+  certificate_arn = data.aws_acm_certificate.wildcard_website.arn
    
   default_action {
     target_group_arn = aws_alb_target_group.tg_load_balancer.id
