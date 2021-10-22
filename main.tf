7c7d7f1a27a90fc69440705ca56cd1182a6be2a7
chatsfeed <83351200+chatsfeed@users.noreply.github.com>
2021-10-22 14:09:57 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -652,8 +652,8 @@ resource "aws_security_group" "sg_load_balancer" {
 # We create a target group for our application load balancer
 resource "aws_alb_target_group" "tg_load_balancer" {
   name     = "target-group-load-balancer"
-  port     = 80
-  protocol = "HTTP"
+  port     = 443
+  protocol = "HTTPS"
   vpc_id   = aws_vpc.vpc.id
 
   depends_on = [
@@ -689,6 +689,32 @@ resource "aws_alb_listener" "listener_load_balancer_http" {
   load_balancer_arn = aws_alb.load_balancer.id
   port              = "80"
   protocol          = "HTTP"
+  
+  default_action {
+    type = "redirect"
+
+    redirect {
+      port        = "443"
+      protocol    = "HTTPS"
+      status_code = "HTTP_301"
+    }
+  }
+ 
+  depends_on = [
+    aws_alb.load_balancer,
+    aws_alb_target_group.tg_load_balancer
+  ]
+}
+
+# We create an https listener for our application load balancer
+resource "aws_alb_listener" "listener_load_balancer_https" {
+  load_balancer_arn = aws_alb.load_balancer.id
+  port              = "443"
+  protocol          = "HTTPS"
+  
+  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  # Default certificate
+  certificate_arn   = data.aws_acm_certificate_validation.wildcard_cert.arn #aws_acm_certificate_validation.wildcard_cert.certificate_arn
    
   default_action {
     target_group_arn = aws_alb_target_group.tg_load_balancer.id
@@ -700,27 +726,6 @@ resource "aws_alb_listener" "listener_load_balancer_http" {
     aws_alb_target_group.tg_load_balancer
   ]
 }
-
-## We create an https listener for our application load balancer
-#resource "aws_alb_listener" "listener_load_balancer_https" {
-  #load_balancer_arn = aws_alb.load_balancer.id
-  #port              = "443"
-  #protocol          = "HTTPS"
-  
-  #ssl_policy        = "ELBSecurityPolicy-2016-08"
-  # Default certificate
-  #certificate_arn   = aws_acm_certificate_validation.wildcard_cert.certificate_arn #aws_acm_certificate_validation.wildcard_cert.certificate_arn
-   
-  #default_action {
-    #target_group_arn = aws_alb_target_group.tg_load_balancer.id
-    #type = "forward"
-  #}
-
-  #depends_on = [
-    #aws_alb.load_balancer,
-    #aws_alb_target_group.tg_load_balancer
-  #]
-#}
 
 
 # We create a security group for our mysql instance
