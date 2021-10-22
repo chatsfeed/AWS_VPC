ea4084cf31eb8d7c1309661fd03aadcd1ed8c451
chatsfeed <83351200+chatsfeed@users.noreply.github.com>
2021-10-22 00:55:26 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -700,25 +700,25 @@ resource "aws_alb_listener" "listener_load_balancer_http" {
 }
 
 ## We create an https listener for our application load balancer
-resource "aws_alb_listener" "listener_load_balancer_https" {
-  load_balancer_arn = aws_alb.load_balancer.id
-  port              = "443"
-  protocol          = "HTTPS"
+#resource "aws_alb_listener" "listener_load_balancer_https" {
+  #load_balancer_arn = aws_alb.load_balancer.id
+  #port              = "443"
+  #protocol          = "HTTPS"
   
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
+  #ssl_policy        = "ELBSecurityPolicy-2016-08"
   # Default certificate
-  certificate_arn   = aws_acm_certificate_validation.wildcard_cert.certificate_arn #aws_acm_certificate_validation.wildcard_cert.certificate_arn
+  #certificate_arn   = aws_acm_certificate_validation.wildcard_cert.certificate_arn #aws_acm_certificate_validation.wildcard_cert.certificate_arn
    
-  default_action {
-    target_group_arn = aws_alb_target_group.tg_load_balancer.id
-    type = "forward"
-  }
+  #default_action {
+    #target_group_arn = aws_alb_target_group.tg_load_balancer.id
+    #type = "forward"
+  #}
 
-  depends_on = [
-    aws_alb.load_balancer,
-    aws_alb_target_group.tg_load_balancer
-  ]
-}
+  #depends_on = [
+    #aws_alb.load_balancer,
+    #aws_alb_target_group.tg_load_balancer
+  #]
+#}
 
 
 # We create a security group for our mysql instance
