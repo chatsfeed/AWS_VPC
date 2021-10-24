57d729be280275d9c24b0bed74d3a51e70351c88
chatsfeed <83351200+chatsfeed@users.noreply.github.com>
2021-10-24 15:17:17 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -721,7 +721,7 @@ resource "aws_alb_listener" "listener_load_balancer_http" {
    
   default_action {
     type             = "forward"
-    target_group_arn =  aws_alb_target_group.tg_load_balancer_http.arn
+    target_group_arn =  aws_instance.wordpress0.arn #aws_alb_target_group.tg_load_balancer_http.arn
   }
  
   depends_on = [
@@ -982,3 +982,95 @@ resource "aws_autoscaling_group" "auto_scaling_wordpress_az_1" {
     #aws_alb_target_group.tg_load_balancer_https
   #]
 #}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# We create a security group for our wordpress instance
+resource "aws_security_group" "sg_wordpress0" {
+  depends_on = [
+    aws_vpc.vpc,
+  ]
+
+  name        = "sg wordpress 0"
+  description = "Allow http inbound traffic"
+  vpc_id      = aws_vpc.vpc.id
+
+  ingress {
+    description = "allow TCP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "allow SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# We create our wordpress instance in public subnet
+resource "aws_instance" "wordpress0" {
+  depends_on = [
+    aws_security_group.sg_wordpress0
+  ]
+  ami = "ami-077e31c4939f6a2f3"
+  instance_type = "t2.micro"
+  key_name = aws_key_pair.public_ssh_key.key_name
+  vpc_security_group_ids = [aws_security_group.sg_wordpress0.id]
+  subnet_id = aws_subnet.public_subnet_1.id
+  user_data = <<EOF
+            #! /bin/bash
+            yum update
+            yum install docker -y
+            systemctl restart docker
+            systemctl enable docker
+            docker pull nginx
+            docker run --name mynginx1 -p 80:80 -d nginx
+  EOF
+
+  tags = {
+      Name = "nginx"
+  }
+}
+
+# We create an elastic IP for our wordpress server
+# A static public IP address that we can assign to our bastion host
+resource "aws_eip" "wordpress_elastic_ip0" {
+   vpc = true
+}
+
+# We associate the elastic ip to our wordpress server
+resource "aws_eip_association" "wordpress_eip_association" {
+  instance_id   = aws_instance.wordpress0.id
+  allocation_id = aws_eip.wordpress_elastic_ip0.id
+}
+
+
+
