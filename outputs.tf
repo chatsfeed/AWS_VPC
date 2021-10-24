9c338d54cfddb0abb9b37700f4b2c0f7f3c23bf9
chatsfeed <83351200+chatsfeed@users.noreply.github.com>
2021-10-24 05:11:59 -0700

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -3,3 +3,7 @@ output "example_name_servers" {
   value = "${aws_route53_zone.main.name_servers}"
 }
 
+# ALB DNS is generated dynamically, return URL so that it can be used
+output "url" {
+  value = "http://${aws_alb.load_balancer.dns_name}/"
+}
