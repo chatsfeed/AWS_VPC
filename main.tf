6321c0430157b27623f8ae9088f6341996f01157
chatsfeed <83351200+chatsfeed@users.noreply.github.com>
2021-10-26 23:38:48 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -757,20 +757,20 @@ resource "aws_alb_listener" "listener_load_balancer_http" {
   port              = "80"
   protocol          = "HTTP"
   
-  default_action {
-    type = "redirect"
-
-    redirect {
-      port        = "443"
-      protocol    = "HTTPS"
-      status_code = "HTTP_301"
-    }
-  }
-   
   #default_action {
-    #type             = "forward"
-    #target_group_arn =   aws_alb_target_group.tg_load_balancer_http.arn
+    #type = "redirect"
+
+    #redirect {
+      #port        = "443"
+      #protocol    = "HTTPS"
+      #status_code = "HTTP_301"
+    #}
   #}
+   
+  default_action {
+    type             = "forward"
+    target_group_arn =   aws_alb_target_group.tg_load_balancer_http.arn
+  }
  
   depends_on = [
     aws_alb.load_balancer,
