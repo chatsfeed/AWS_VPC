c886bfc6a91cbf7416497f2db04e031336691c5d
chatsfeed <83351200+chatsfeed@users.noreply.github.com>
2021-10-26 21:49:56 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -821,7 +821,7 @@ resource "aws_alb_listener_rule" "listener_load_balancer_rule_http" {
    
   condition {
     host_header {
-      values = [var.website-domain, var.www-website-domain, var.app-website-domain] #["${var.www-website-domain}"]
+      values = [var.www-website-domain] 
     }
  }
    
