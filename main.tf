f15061d847c2a53b6fb5ca22582279ee440c8029
chatsfeed <83351200+chatsfeed@users.noreply.github.com>
2021-10-28 13:52:42 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -802,12 +802,12 @@ resource "aws_alb_listener" "listener_load_balancer_http" {
 
 
 resource "aws_alb_listener_rule" "listener_load_balancer_rule_http" {
-  depends_on   = [aws_alb_target_group.tg_load_balancer_http]  
+  depends_on   = [aws_alb_target_group.tg_load_balancer_http_www]  
   listener_arn = aws_alb_listener.listener_load_balancer_http.arn
   #priority     = 100   
   action {    
     type             = "forward"    
-    target_group_arn = "${aws_alb_target_group.tg_load_balancer_http.id}"  
+    target_group_arn = "${aws_alb_target_group.tg_load_balancer_http_www.id}"  
   }  
    
   condition {
@@ -820,12 +820,12 @@ resource "aws_alb_listener_rule" "listener_load_balancer_rule_http" {
 
 
 resource "aws_alb_listener_rule" "listener_load_balancer_rule_root_http" {
-  depends_on   = [aws_alb_target_group.tg_load_balancer_http]  
+  depends_on   = [aws_alb_target_group.tg_load_balancer_http_www]  
   listener_arn = aws_alb_listener.listener_load_balancer_http.arn
   #priority     = 100   
   action {    
     type             = "forward"    
-    target_group_arn = "${aws_alb_target_group.tg_load_balancer_http.id}"  
+    target_group_arn = "${aws_alb_target_group.tg_load_balancer_http_www.id}"  
   }  
    
   condition {
@@ -838,12 +838,12 @@ resource "aws_alb_listener_rule" "listener_load_balancer_rule_root_http" {
 
 
 resource "aws_alb_listener_rule" "listener_load_balancer_rule_app_http" {
-  depends_on   = [aws_alb_target_group.tg_load_balancer_http]  
+  depends_on   = [aws_alb_target_group.tg_load_balancer_http_app]  
   listener_arn = aws_alb_listener.listener_load_balancer_http.arn
   #priority     = 100   
   action {    
     type             = "forward"    
-    target_group_arn = "${aws_alb_target_group.tg_load_balancer_http.id}"  
+    target_group_arn = "${aws_alb_target_group.tg_load_balancer_http_app.id}"  
   }  
    
   condition {
@@ -919,12 +919,12 @@ resource "aws_alb_listener" "listener_load_balancer_https" {
 
 
 resource "aws_alb_listener_rule" "listener_load_balancer_rule_https" {
-  depends_on   = [aws_alb_target_group.tg_load_balancer_https]  
+  depends_on   = [aws_alb_target_group.tg_load_balancer_https_www]  
   listener_arn = aws_alb_listener.listener_load_balancer_https.arn
   #priority     = 100   
   action {    
     type             = "forward"    
-    target_group_arn = "${aws_alb_target_group.tg_load_balancer_https.id}"  
+    target_group_arn = "${aws_alb_target_group.tg_load_balancer_https_www.id}"  
   }  
    
   condition {
@@ -937,12 +937,12 @@ resource "aws_alb_listener_rule" "listener_load_balancer_rule_https" {
 
 
 resource "aws_alb_listener_rule" "listener_load_balancer_rule_root_https" {
-  depends_on   = [aws_alb_target_group.tg_load_balancer_https]  
+  depends_on   = [aws_alb_target_group.tg_load_balancer_https_www]  
   listener_arn = aws_alb_listener.listener_load_balancer_https.arn
   #priority     = 100   
   action {    
     type             = "forward"    
-    target_group_arn = "${aws_alb_target_group.tg_load_balancer_https.id}"  
+    target_group_arn = "${aws_alb_target_group.tg_load_balancer_https_www.id}"  
   }  
    
   condition {
@@ -956,12 +956,12 @@ resource "aws_alb_listener_rule" "listener_load_balancer_rule_root_https" {
 
 
 resource "aws_alb_listener_rule" "listener_load_balancer_rule_app_https" {
-  depends_on   = [aws_alb_target_group.tg_load_balancer_https]  
+  depends_on   = [aws_alb_target_group.tg_load_balancer_https_app]  
   listener_arn = aws_alb_listener.listener_load_balancer_https.arn
   #priority     = 100   
   action {    
     type             = "forward"    
-    target_group_arn = "${aws_alb_target_group.tg_load_balancer_https.id}"  
+    target_group_arn = "${aws_alb_target_group.tg_load_balancer_https_app.id}"  
   }  
    
   condition {
