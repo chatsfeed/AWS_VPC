6249611dea4931351fe520e3cd5ffd61e05cbee5
chatsfeed <83351200+chatsfeed@users.noreply.github.com>
2021-10-30 22:35:51 -0700

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,20 @@ output "example_name_servers" {
 output "url" {
   value = "http://${aws_alb.load_balancer.dns_name}/"
 }
+
+
+output "bastion_host_1_public_ip" {  
+  value = "${aws_eip.bastion_elastic_ip_1.public_ip}"
+}
+
+output "bastion_host_1_private_ip" {  
+  value = "${aws_instance.bastion_host_1.private_ip}"
+}
+
+output "bastion_host_2_public_ip" {  
+  value = "${aws_eip.bastion_elastic_ip_2.public_ip}"
+}
+
+output "bastion_host_2_private_ip" {  
+  value = "${aws_instance.bastion_host_2.private_ip}"
+}
